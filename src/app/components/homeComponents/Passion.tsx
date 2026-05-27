ed0ef46b25d5109a0d054cc8af00d1652d919de6
Oliver <oliverbengtsson67@gmail.com>
2026-05-28 01:51:01 +0200

    ++

--- src/app/components/homeComponents/Passion.tsx
+++ src/app/components/homeComponents/Passion.tsx
@@ -13,25 +13,68 @@ const Passion = ({ page }: { page: string }) => {
             <TextBox tagheader="Passion" header="Hey Yum! Only the good">
               <div className="grid ">
                 <div className=" flex flex-col gap-6 my-4">
-                  <p>We’re HEY YUM! and we make yummilicious ORGANIC candy glowing with good vibes.</p>
-                  <p>We’re a young Danish company on a journey to redefine candy and offer the world an alternative – one that’s ORGANIC, unjunked and just better.</p>
-                  <p>It started with a sweet idea. We wanted to make a whimsical universe of gummilicious magic. Something wonderfully soft and wickedly chewy, free from artificial flavours, colours, sweeteners and preservatives. So we got busy in the kitchen and stirred up a tasty treat that’s flavoured with fruit and mixed with sweet intentions.</p>
-                  <p className="mb-8">Each bright kind of ORGANIC, juicy HEY YUM! candy is a whimsical universe of gummilicious magic where everyone is welcome.</p>
-                  {page === "home" && <LinkButton aria-label="Read more about HeyYum" color="--black" link="aboutUs" text="Read more" />}
+                  <p>
+                    We’re HEY YUM! and we make yummilicious ORGANIC candy
+                    glowing with good vibes.
+                  </p>
+                  <p>
+                    We’re a young Danish company on a journey to redefine candy
+                    and offer the world an alternative – one that’s ORGANIC,
+                    unjunked and just better.
+                  </p>
+                  <p>
+                    It started with a sweet idea. We wanted to make a whimsical
+                    universe of gummilicious magic. Something wonderfully soft
+                    and wickedly chewy, free from artificial flavours, colours,
+                    sweeteners and preservatives. So we got busy in the kitchen
+                    and stirred up a tasty treat that’s flavoured with fruit and
+                    mixed with sweet intentions.
+                  </p>
+                  <p className="mb-8">
+                    Each bright kind of ORGANIC, juicy HEY YUM! candy is a
+                    whimsical universe of gummilicious magic where everyone is
+                    welcome.
+                  </p>
+                  {page === "home" && (
+                    <LinkButton
+                      aria-label="Read more about HeyYum"
+                      color="--black"
+                      link="aboutUs"
+                      text="Read more"
+                    />
+                  )}
                 </div>
               </div>
             </TextBox>
           </div>
-          <div className="relative w-full h-[240] md:h-[660] order-1 ">
-            <Image src="/assets/images/home/all_flavors_homeenv_shot.png" fetchPriority="high" alt="Social Media image" fill className="object-cover object-center" />
+          <div className="relative w-full h-[240] md:h-[660] order-1">
+            <Image
+              src="/assets/images/home/all_flavors_homeenv_shot.png"
+              fetchPriority="high"
+              alt="Social Media image"
+              fill
+              className="object-cover object-center p-4"
+            />
           </div>
         </div>
         <div className="flex md:flex-col md:gap-[40] p-4">
           <div className="relative w-full h-[167] md:h-full mt-auto">
-            <Image src="/assets/images/home/basketOfYum.png" fetchPriority="high" alt="Social Media image" fill className="object-cover object-center" />
+            <Image
+              src="/assets/images/home/basketOfYum.png"
+              fetchPriority="high"
+              alt="Social Media image"
+              fill
+              className="object-cover object-center"
+            />
           </div>
           <div className="relative w-full h-[204] md:h-full">
-            <Image loading="lazy" src="/assets/images/home/handOfGummies.png" alt="Social Media image" fill className="object-cover object-center" />
+            <Image
+              loading="lazy"
+              src="/assets/images/home/handOfGummies.png"
+              alt="Social Media image"
+              fill
+              className="object-cover object-center"
+            />
           </div>
         </div>
       </div>
