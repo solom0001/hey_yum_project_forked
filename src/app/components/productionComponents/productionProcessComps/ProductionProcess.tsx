9f8b55a26c03a0aa2fd407cc2808024272bfbcb9
Oliver <oliverbengtsson67@gmail.com>
2026-06-02 13:35:35 +0200

    ++

--- src/app/components/productionComponents/productionProcessComps/ProductionProcess.tsx
+++ src/app/components/productionComponents/productionProcessComps/ProductionProcess.tsx
@@ -4,22 +4,45 @@ import { div } from "framer-motion/client";
 
 const ProductionProcess = () => {
   return (
-    <div className="fullbleed bg-(--black) ContentWitdh">
+    <div className="fullbleed bg-(--black) ContentWitdh overflow-clip">
       <div className="flex flex-col w-full md:grid grid-cols-2 gap-8  text-(--white)!">
         {/*Sticky image */}
-        <div className="relative w-full h-full bg-[url(/assets/images/production/rainbow_bg2.svg)]  bg-contain bg-center bg-no-repeat">
-          <div className="md:sticky top-0  md:aspect-auto md:w-[70%] mx-auto overflow-hidden grid *:col-1! *:row-1">
-            <h1 className="mt-10 pt-8 text-[2.5rem]! md:text-(--black) relative z-10 text-center">The making of Hey Yum!</h1>
+        <div className="relative w-full h-[101%] md:bg-[url(/assets/images/production/rainbow_bg2.svg)]  bg-contain bg-center bg-no-repeat">
+          <div className="md:sticky top-[25%]  md:aspect-auto md:w-[70%] mx-auto overflow-hidden grid *:col-1! *:row-1">
+            <h1 className="mt-10 pt-8 text-[2.5rem]! md:text-(--black) relative z-10 text-center">
+              The making of Hey Yum!
+            </h1>
             <div className="hidden md:block relative w-full aspect-square">
-              <Image loading="lazy" src="/assets/images/production/heyyum_monster.png" alt="image" fill className="object-center object-cover" />
+              <Image
+                loading="lazy"
+                src="/assets/images/production/heyyum_monster.png"
+                alt="image"
+                fill
+                className="object-center object-cover"
+              />
             </div>
           </div>
         </div>
         {/*Steps */}
         <div className="flex flex-col h-full gap-12 pt-8 ">
-          <StepComp title="Heating" src="prod_step1.png" step="1" text="Sugar, glucose syrup and gelling agent is carefully heated until gelling. Then citric acid is added becauce it brings richness to the fruity flavours. Then we add organic fruit and plant juice concentrates and high-quality natural flavourings and extracts." />
-          <StepComp title="Molding" src="molds_heyYum.jpg" step="2" text=" A board of molds is pressed into wooden trays. The liquid mass then gets filled in those small wells created by the moulds. The gummy candies are then lying flat on their bellies until they get dry and keep their shape. In this way, six different colored fruit gums can be made at the same time." />
-          <StepComp title="Coating" src="prod_step_3.jpg" step="3" text="When the the fruit gums are dry, they are taken out of their bed. The finished gummy candies are either coated with plant-based carnauba wax to give them a shiny appearance and prevent them from sticking together or they are coated with a sour sugar crystals like Sour Flower." />
+          <StepComp
+            title="Heating"
+            src="prod_step1.png"
+            step="1"
+            text="Sugar, glucose syrup and gelling agent is carefully heated until gelling. Then citric acid is added becauce it brings richness to the fruity flavours. Then we add organic fruit and plant juice concentrates and high-quality natural flavourings and extracts."
+          />
+          <StepComp
+            title="Molding"
+            src="molds_heyYum.jpg"
+            step="2"
+            text=" A board of molds is pressed into wooden trays. The liquid mass then gets filled in those small wells created by the moulds. The gummy candies are then lying flat on their bellies until they get dry and keep their shape. In this way, six different colored fruit gums can be made at the same time."
+          />
+          <StepComp
+            title="Coating"
+            src="prod_step_3.jpg"
+            step="3"
+            text="When the the fruit gums are dry, they are taken out of their bed. The finished gummy candies are either coated with plant-based carnauba wax to give them a shiny appearance and prevent them from sticking together or they are coated with a sour sugar crystals like Sour Flower."
+          />
         </div>
       </div>
     </div>
--- src/app/components/productionComponents/productionProcessComps/StepComp.tsx
+++ src/app/components/productionComponents/productionProcessComps/StepComp.tsx
@@ -1,27 +1,33 @@
 import Image from "next/image";
 
-type Props={
-    text:string;
-    step:string;
-    title:string;
-    src:string;
-}
+type Props = {
+  text: string;
+  step: string;
+  title: string;
+  src: string;
+};
 
-const StepComp = ({text,step,title,src}:Props) => {
+const StepComp = ({ text, step, title, src }: Props) => {
   return (
     <div className="flex flex-col gap-4 p-4 Textbox_width h-[500] ">
       <div className="w-full grid grid-cols-[1fr_2fr] md:grid-cols-2 h-fit">
         <span className="w-full flex flex-col justify-end ">
-          <p className="text-[35px]!">{step}</p>
+          <p className="text-[65px]!">{step}</p>
           <h3>{title}</h3>
         </span>
 
         <div className="relative aspect-square! md:aspect-auto w-full overflow-hidden">
-          <Image loading="lazy" src={`/assets/images/production/${src}`} alt="image" fill className="object-center object-cover" />
+          <Image
+            loading="lazy"
+            src={`/assets/images/production/${src}`}
+            alt="image"
+            fill
+            className="object-center object-cover"
+          />
         </div>
       </div>
-<p>{text}</p>    
-</div>
+      <p>{text}</p>
+    </div>
   );
 };
 
