fd2b2485abde12a157d1078462dc7f8e5d683910
Oliver <oliverbengtsson67@gmail.com>
2026-06-02 13:05:48 +0200

    ++

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,18 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans, Lilita_One } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/utilityComponents/Footer";
+import localFont from "next/font/local";
+
+export const heyYumFont = localFont({
+  src: [
+    {
+      path: "../app/fonts/Hey_yum_font-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+  variable: "--font-heyyum",
+});
 
 const plusJakarta = Plus_Jakarta_Sans({
   variable: "--font-plus-jakarta",
@@ -28,7 +40,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${plusJakarta.variable} ${lilitaOne.variable} h-full antialiased`}
+      className={`${plusJakarta.variable} ${lilitaOne.variable} ${heyYumFont.variable} h-full antialiased`}
     >
       <body className="min-h-full grid">
         {children}
